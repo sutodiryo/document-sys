21aff206f1f0983984dca7b329c5f60802556d45
sutodiryo <hubungi.yogi@gmail.com>
2024-10-18 10:37:40 +0700

    x

--- resources/views/livewire/file/view.blade.php
+++ resources/views/livewire/file/view.blade.php
@@ -1,4 +1,4 @@
 <div>
-    <iframe style="width: 100%; min-height:500px"
+    <iframe style="width: 100%; height:5000px"
         src="https://docs.google.com/gview?url={{ $link_file }}&embedded=true"></iframe>
 </div>
